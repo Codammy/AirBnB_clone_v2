2cf358e42e598dcebf5c18e5bec21679da3e5bcf
Dami <damisco005@gmail.com>
2024-05-20 04:13:31 +0100

    templates __pycache__

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -51,7 +51,7 @@ def render_number(n):
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    """display a HTML page only if n is an integer"""
+    """displays a HTML page only if n is an integer"""
     return render_template('6-number_odd_or_even.html', n=n)
 
 
