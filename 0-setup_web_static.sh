cdce82391f6909498c3c216beeb35278e4dfbd02
Dami <damisco005@gmail.com>
2024-05-25 22:56:17 +0100

    ubuntu@54.144.249.31~ 0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Sets up my web servers for the deployment of web_static.
+
+apt-get install nginx
+mkdir -p /data/web_static/releases/test /data/web_static/shared/
+echo 'Dami test file' > /data/web_static/releases/test/index.html
+ln -fs /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data
+echo 'server {
+	listen [::]:80 default_server;
+
+	root var/www/html;
+
+	server_name _;
+
+	location /hbnb_static {
+	alias /data/web_static/current;
+	}
+}
+' > /etc/nginx/sites-available/default;
+nginx -s reload;
