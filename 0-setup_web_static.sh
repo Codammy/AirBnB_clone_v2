9f10ac3b8de6017e4d800213ab01f4a10acedfbd
Dami <damisco005@gmail.com>
2024-05-26 13:42:40 +0100

    0-setup_web_static.sh ubuntu@54.144.249.31~

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,5 +6,5 @@ mkdir -p /data/web_static/releases/test /data/web_static/shared/
 echo 'Dami test file' > /data/web_static/releases/test/index.html
 ln -fs /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data
-sed -i '0, /location/s//location \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}\n\t&/' /etc/nginx/sites-available/default
+#sed -i '0, /location/s//location \/hbnb_static {\n\t\talias \/data\/web_static\/current;\n\t}\n\t&/' /etc/nginx/sites-available/default
 service nginx restart
