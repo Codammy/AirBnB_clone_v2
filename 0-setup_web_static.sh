2ce8864944d7e811734a596ca509ec6c79ccff38
Dami <damisco005@gmail.com>
2024-05-26 13:35:30 +0100

    0-setup_web_static.sh sudo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-# Sets up my web servers for the deployment of web_static.
 
-apt update
 apt install nginx
 mkdir -p /data/web_static/releases/test /data/web_static/shared/
 echo 'Dami test file' > /data/web_static/releases/test/index.html
