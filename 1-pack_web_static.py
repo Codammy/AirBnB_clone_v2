2d941d7b798ce13d57b69227946e688ef7a11b92
Dami <damisco005@gmail.com>
2024-05-27 08:41:28 +0100

    versions __pycache__

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,7 +4,7 @@ Fabric script that generates a .tgz archive from
 the contents of the web_static folder
 """
 from datetime import datetime
-from fabric.api import *
+from fabric.api import (prefix, local)
 
 
 def do_pack():
@@ -12,7 +12,7 @@ def do_pack():
     generates a .tgz archive from the contents of the web_static folder
     """
     dt = datetime.now()
-    tarfile = 'web_static_{}{}{}{}{}{}.tar.gz'.format(
+    tarfile = 'web_static_{}{}{}{}{}{}.tgz'.format(
             dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second
             )
     with prefix('mkdir -p versions'):
